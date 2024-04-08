5dd66f00f67e235e9f7c68e0e61a701ca5b720f0
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-04-08 20:46:18 +0200

    added move file feature

--- file_management.py
+++ file_management.py
@@ -1,14 +1,39 @@
+"""Module responsible for file management (renaming, moving)"""
 import os
+import shutil
 
 class FileManagement:
+    """Class responsible for file management"""
     def __init__(self, destination_dir: str, file_name: str) -> None:
+        """Initializes FileManagement object.
+
+        Args:
+            destination_dir (str): File's directory path
+            file_name (str): File name with its extension.
+        """
         self.destination_dir = destination_dir
         self.file_name = file_name
-        self.full_path = os.path.join(destination_dir, file_name)
 
-    def create_unique_filename(self):
+    def create_unique_filename(self) -> str:
+        """Create unique filename by adding unique number at the end of file name.
+
+        Returns:
+            str: The proper file path.
+        """
         name, extension = self.file_name.split()
-        counter = 1
+        path = os.path.join(self.destination_dir, self.file_name)
+        counter =+ 1
+        while os.path.exists(path):
+            new_name = f"{name}{str(counter)}.{extension}"
+            path = os.path.join(self.destination_dir, new_name)
+            counter =+ 1
+        return str(path)
+    
+    def move_file(self, source_path: str) -> None:
+        """Moves file from source directory to the destination directory.
 
-        while os.path.exists(self.full_path):
-            pass
+        Args:
+            source_path (str): The path from file should be moved.
+        """
+        unique_destination_path = self.create_unique_filename()
+        shutil.move(source_path, unique_destination_path)
