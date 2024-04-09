c0a1f4ca0361eaeffc452c2cf5bb3b6a9d0aaee4
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-04-09 21:08:06 +0200

    added move handler

---
+++ move_handler.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from watchdog.events import FileSystemEventHandler
+from file_management import FileManagement
+
+class MoveHandler(FileSystemEventHandler):
+    def __init__(self, file_mapping: dict, desktop_path: str) -> None:
+        self.file_mapping = file_mapping
+        self.desktop_path = desktop_path
+    
+    def on_created(self, event):
+        source_path = event.src_path
+        file_name = os.path.basename(source_path)
+        ext = os.path.splitext(file_name)[1].lower()
+        for folder, extensions in self.file_mapping.items():
+            if ext in extensions:
+                target_folder = os.path.join(self.desktop_path, folder)
+                file_manager = FileManagement(target_folder, file_name)
+                file_manager.move_file(source_path)
