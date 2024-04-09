677eca2231c460836a4dd02a63dade313b7d9f97
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-04-09 21:08:06 +0200

    added move handler

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
+import time
+from utils import categorizes_files
+from move_handler import MoveHandler
+from watchdog.observers import Observer
 
-path = r"C:\Users\kubag\Desktop\Bank_Raport.xlsm"
 
-print(os.path.basename(path))
+if __name__ == "__main__":
+    desktop_path = r"C:\Users\kubag\Desktop"
+    file_mapping = categorizes_files()
+    event_handler = MoveHandler(file_mapping, desktop_path)
+    observer = Observer()
+    observer.schedule(event_handler, desktop_path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
